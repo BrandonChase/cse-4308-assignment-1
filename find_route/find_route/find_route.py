4315fe706bfd3bdb188fbf8303968676ed06ae8c
BrandonChase <coding@brandonchase.me>
2018-01-25 17:27:49 -0600

    Turns tree variable into a PathFinder class
    This will better encapsulate code later on so I call functions like pf.do_x_search_algorithm, pf.print_tree().

--- find_route/find_route/find_route.py
+++ find_route/find_route/find_route.py
@@ -6,28 +6,38 @@ class Path(object):
         self.distance = distance
 
     def __repr__(self):
-        return "%s: %d miles" % (self.destination, self.distance)
+        return "to %s: %d miles" % (self.destination, self.distance)
+class PathFinder(object):
+    def __init__(self, filename):
+        self.load_tree(filename)
 
-def load_tree(filename):
-    tree = {}
-    with open(filename, "r") as input_file:
-        lines =  input_file.readlines()
-        for line in lines:
-            if line == "END OF INPUT\n":
-                break;
-            else:
-                items = line.split()
-                #add path from origin to destination
-                tree.setdefault(items[0],[]).append(Path(items[1], int(items[2])))
-                #add path from destination to origin since it is a two way street
-                tree.setdefault(items[1],[]).append(Path(items[0], int(items[2])))
+    def __repr__(self):
+        result = ""
+        for city in self.tree:
+            result += city + ":\n"
+            for path in self.tree[city]:
+                result += "\t" + repr(path) + "\n"
+        return result
 
-    return tree
+    def load_tree(self, filename):
+        tree = {}
+        with open(filename, "r") as input_file:
+            lines =  input_file.readlines()
+            for line in lines:
+                if line == "END OF INPUT\n":
+                    break;
+                else:
+                    items = line.split()
+                    #add path from origin to destination
+                    tree.setdefault(items[0],[]).append(Path(items[1], int(items[2])))
+                    #add path from destination to origin since it is a two way street
+                    tree.setdefault(items[1],[]).append(Path(items[0], int(items[2])))
+        self.tree = tree
 
 def main(args):
     #load paths into list of paths
-    tree = load_tree(args[1])
-    print tree
+    pf = PathFinder(args[1])
+    print pf
     #find path given origin and destination
     #print results
     pass
