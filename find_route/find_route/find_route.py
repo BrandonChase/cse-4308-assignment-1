3732b36900d06c3d3d64714c7ac3bfee41536ca8
BrandonChase <coding@brandonchase.me>
2018-01-25 16:27:37 -0600

    Adds program layout, path class, and ignores VS files

---
+++ find_route/find_route/find_route.py
@@ -0,0 +1,15 @@
+import sys
+
+class path(object):
+    def __init__(self, origin, destination, distance):
+        self.origin = origin
+        self.destination = destination
+        self.distance = distance
+
+def main(args):
+    #load paths into list of paths
+    #find path given origin and destination
+    #print results
+
+if __name__ =='__main__':
+    main(sys.argv)
